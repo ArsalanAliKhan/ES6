927f97c05fc4ba3388092fd7b522534c7cf1f91b
ArsalanAliKhan <50230521+ArsalanAliKhan@users.noreply.github.com>
2019-05-06 18:14:07 +0500

    Update book.js

--- book.js
+++ book.js
@@ -1,21 +1,23 @@
-
-var book = ["HTML tags","CSS classes","Js.net"];
+var books = ["HTML Tags","CSS Classes","Js.net"];
 var x = 1000;
 var y = 2000;
 var z = x + y;
-var answer1 = "300 pages";
+var d = "1st-Mar-2019";
+var answer1 = "600 pages";
 var answer2 = "450 pages";
-var answer3 = '500 pages';
+var answer3 = '300 pages';
 var a = false;        
 var b = new Boolean(true);
 var person = {
-  firstName: "Saleem Ahmad",
+  firstName: "Saleem Ahmed",
   lastName : "Khalid Ali",
   id     :  5566 ,
 };
 
-console.log(cars);
+
+console.log(books);
 document.getElementById("demo").innerHTML = 
-"Book Name: " + book[0] + "<br>" + "Book price: " + "Rs." + z + "<br>" + "In this book: " 
-+ answer3 + "<br>" + "Book writer name: " + person.firstName + "<br>" + "Made in England: " 
+"Book Name: " + books[0] + "<br>" + "Book Writer Name: " + person.firstName + "<br>" + 
+"Book Publish Date: " + d + "<br>" + "Book price: " + "Rs." + z + "<br>" + "In this book: " 
++ answer1 + "<br>" + "Made in England: " 
 + (a==b);
